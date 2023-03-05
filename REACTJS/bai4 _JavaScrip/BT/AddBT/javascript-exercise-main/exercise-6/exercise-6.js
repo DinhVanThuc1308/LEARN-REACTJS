9d1c66bd2e79be78132bf2353b8b54a1c79144a8
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2023-03-06 03:31:58 +0700

    a

--- REACTJS/bai4 _JavaScrip/BT/AddBT/javascript-exercise-main/exercise-6/exercise-6.js
+++ REACTJS/bai4 _JavaScrip/BT/AddBT/javascript-exercise-main/exercise-6/exercise-6.js
@@ -1,3 +1,16 @@
 function convertNumber(number) {
-  // Coding here
+  var Result = "";
+  var con_lai;
+  while (number > 0) {
+    con_lai = number % 2;
+    Result += con_lai;
+    number = (number - con_lai) / 2;
+  }
+  Result = Result.split('');
+  Result = Result.reverse();
+  Result = Result.join('');
+  return Result;
 }
+
+a = convertNumber(47); // => 101111
+console.log(a);
