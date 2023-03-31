a90688c65784d5b31d1033f7f238d2bc638a9981
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2023-03-31 13:38:08 +0700

    TH1234

--- REACTJS/Bai8_React/Test/my-app/src/App.js
+++ REACTJS/Bai8_React/Test/my-app/src/App.js
@@ -1,19 +1,77 @@
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   const todo = ["hello", "world"];
   return (
-    // bài 1
+    // bài tập 1
     // const isUserLoggedIn = true;
     // return <h1>{isUserLoggedIn ? "Hello User" : "Hello Guest"}</h1>;
 
-    //Bài 2
-    <h1>
-      {todo.map((item, index) => {
-        return <p key={index}> {item}</p>;
-      })}
-    </h1>
+    //Bài tập 2
+    //   <h1>
+    //     {todo.map((item, index) => {
+    //       return <p key={index}> {item}</p>;
+    //     })}
+    //   </h1>
+    // );
+
+    // // Thực hành 1
+
+    // <div>Hello world</div>
+    // Thực hành 2
+    //   <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    //Thực hành 3
+    // <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
+
+    //Thực hành 4
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 export default App;
