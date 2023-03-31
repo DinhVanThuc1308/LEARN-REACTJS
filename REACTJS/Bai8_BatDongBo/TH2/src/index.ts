b2ca51836566ad0664399cc347f41c881ab1fd6f
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2023-03-31 13:29:45 +0700

    TH4

--- REACTJS/Bai8_BatDongBo/TH2/src/index.ts
+++ REACTJS/Bai8_BatDongBo/TH2/src/index.ts
@@ -17,5 +17,18 @@
 // }
 
 // getUserProfile();
+// bài thực hành 4
+import axios from 'axios';
 
+const getData = async () => {
+    try {
+        const listPost = await axios.get('http://jsonplaceholder.typicode.com/posts');
+        console.log(listPost.data[0]);
+        const dataDetail = await axios.get('http://jsonplaceholder.typicode.com/posts/1');
+        console.log(dataDetail.data);
+    } catch (err) {
+        console.log(err);
+    }
+}
 
+getData();
