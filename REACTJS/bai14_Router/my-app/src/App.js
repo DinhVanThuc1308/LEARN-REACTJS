126b0e14d2eac1cf9f8b4136c321726ea95fe9b6
vanthuc1308 <20010927@st.phenikaa-uni.edu.vn>
2023-04-20 00:16:58 +0700

    new comiit

---
+++ REACTJS/bai14_Router/my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Bai1 from "./Bai1";
+import Bai2 from "./Bai2";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bai1</h1>
+      <Bai1 />
+      <h1>Bai2</h1>
+      <Bai2 />
+    </div>
+  );
+}
+
+export default App;
